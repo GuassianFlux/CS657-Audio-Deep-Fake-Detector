af9d5cfadc549aab554904a5a4537c826297306e
branch.hill <branch.hill@magnolia-river.com>
2023-09-05 11:26:33 -0500

    Implement methods for training and saving models

--- src/deepfake_detector/deepfake_detector.py
+++ src/deepfake_detector/deepfake_detector.py
@@ -1,4 +1,5 @@
 from model_utilities.model_utils import Model_Utils
+import tensorflow as tf 
 
 class DeepFake_Detector:
     def __init__(self):
@@ -7,7 +8,8 @@ class DeepFake_Detector:
     def load_model(self, models_dir, model_id):
         self.loaded_model = Model_Utils.load_model(models_dir, model_id)
 
-    # def predict(self, test_spectrogram_ds):
-    def predict(self):
-        print("Not Implemented")
+    def predict(self, test_spectrogram_ds):
+        self.loaded_model.evaluate(test_spectrogram_ds, return_dict=True)
+        y_hat = self.loaded_model.predict(test_spectrogram_ds)
+        print(y_hat)
     

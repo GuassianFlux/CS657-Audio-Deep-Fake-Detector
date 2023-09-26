ecb49019787d0375717ac41cdf90bd4ba7e1d4e6
branch.hill <branch.hill@magnolia-river.com>
2023-09-25 21:26:19 -0500

    display activations with keract

--- src/deepfake_detector/deepfake_detector.py
+++ src/deepfake_detector/deepfake_detector.py
@@ -7,7 +7,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from model_utilities.model_utils import Model_Utils
 from termcolor import colored
+from keract import get_activations, display_activations
 import numpy as np
+import shutil
 
 class DeepFake_Detector:
     def __init__(self):
@@ -19,8 +21,32 @@ class DeepFake_Detector:
     def set_model(self, model):
         self.loaded_model = model
 
-    def predict_dataset(self, test_spectrogram_ds, class_names):
-        print("Making predictions for test dataset...")
+    def _log_outputs(self, sample, prediction_dir):
+        inp = self.loaded_model.input
+        outputs = [layer.output for layer in self.loaded_model.layers]
+        functors = [tf.keras.backend.function([inp], [out]) for out in outputs]
+        layer_outs = [func([sample]) for func in functors]
+        file_name = "output.txt"
+        output_file = os.path.join(prediction_dir, file_name)
+        with open(output_file, 'w') as file:
+            for idx, layer in enumerate(self.loaded_model.layers):
+                print("layer name {} \noutputs: {}". format(layer.name, numpy.array(layer_outs[idx])), file=file)
+
+    def _show_ouputs(self, input, prediction_dir):
+        activations = get_activations(self.loaded_model, input) #, auto_compile=True)
+        display_activations(activations, cmap='YlGnBu', save=True, directory=prediction_dir)
+
+    def build_prediction_path(self, prediction_dir):
+        if os.path.exists(prediction_dir):
+            try:
+                shutil.rmtree(prediction_dir) 
+            except OSError as e:
+                print(f"Error in build_model_path: {e}")
+        os.makedirs(prediction_dir)
+
+    def predict_dataset(self, test_spectrogram_ds, class_names, prediction_dir):
+        print("Making dataset predictions...")
+        self.build_prediction_path(prediction_dir)
         file_num = 1
         correct = 0
         batch_size = 0
@@ -39,7 +65,8 @@ class DeepFake_Detector:
                     correct += 1
                 print(colored(f'WAV {file_num} Prediction: {class_names[predicted]}, Actual: {class_names[actual]}, Confidence: {confidence_str} ', print_color))
                 file_num += 1
-            break
+
+            #self._show_ouputs(X[1:2], prediction_dir)
 
         print(f'Number correct: {correct} out of {batch_size}')
         print(f'Accuracy: {format(correct / batch_size, ".4%")}')

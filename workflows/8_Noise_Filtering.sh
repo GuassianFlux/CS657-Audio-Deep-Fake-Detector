5df67e644edcf9e257b9899fefe5a70c64f92351
Jacob Pomatto <jd.gilley@yahoo.com>
2023-11-26 16:35:35 -0600

    Update noise filtering script

--- workflows/8_Noise_Filtering.sh
+++ workflows/8_Noise_Filtering.sh
@@ -1,33 +1,61 @@
 #!/bin/bash
 
 # Workflow 8 - Noise Filtering: Filtering the white noise dataset
-echo "=== Workflow 8 - Noise Filtering: Filtering the white noise dataset ==="
+echo "=== Workflow 8 - Noise Filtering: Filtering the white and burst noise datasets ==="
 
 
 real_white_noise_dataset_path='/workspaces/white_noise_data_sets/real'
 fake_white_noise_dataset_path='/workspaces/white_noise_data_sets/fake'
 
 
-root_filtered_noise_dir='/workspaces/filtered_noise_data_sets'
-if [ -d $root_filtered_noise_dir ]; then
-    rm -r $root_filtered_noise_dir
+root_filtered_white_noise_dir='/workspaces/filtered_white_noise_data_sets'
+if [ -d $root_filtered_white_noise_dir ]; then
+    rm -r $root_filtered_white_noise_dir
 fi
-mkdir $root_filtered_noise_dir
+mkdir $root_filtered_white_noise_dir
 
-real_filtered_noise_dir='/workspaces/filtered_noise_data_sets/real'
-if [ -d $real_filtered_noise_dir ]; then
-    rm -r $real_filtered_noise_dir
+real_filtered_white_noise_dir='/workspaces/filtered_white_noise_data_sets/real'
+if [ -d $real_filtered_white_noise_dir ]; then
+    rm -r $real_filtered_white_noise_dir
 fi
-mkdir $real_filtered_noise_dir
+mkdir $real_filtered_white_noise_dir
 
-fake_filtered_noise_dir='/workspaces/filtered_noise_data_sets/fake'
-if [ -d $fake_filtered_noise_dir ]; then
-    rm -r $fake_filtered_noise_dir
+fake_filtered_white_noise_dir='/workspaces/filtered_white_noise_data_sets/fake'
+if [ -d $fake_filtered_white_noise_dir ]; then
+    rm -r $fake_filtered_white_noise_dir
 fi
-mkdir $fake_filtered_noise_dir
+mkdir $fake_filtered_white_noise_dir
 
 # Filter noise for real data
-python /workspaces/src/filter_noise_dataset.py $real_white_noise_dataset_path $real_filtered_noise_dir 
+python /workspaces/src/filter_noise_dataset.py $real_white_noise_dataset_path $real_filtered_white_noise_dir 
 
 # Filter noise for fake data
-python /workspaces/src/filter_noise_dataset.py $fake_white_noise_dataset_path $fake_filtered_noise_dir 
+python /workspaces/src/filter_noise_dataset.py $fake_white_noise_dataset_path $fake_filtered_white_noise_dir 
+
+real_burst_noise_dataset_path='/workspaces/burst_noise_data_sets/real'
+fake_burst_noise_dataset_path='/workspaces/burst_noise_data_sets/fake'
+
+
+root_filtered_burst_noise_dir='/workspaces/filtered_burst_noise_data_sets'
+if [ -d $root_filtered_burst_noise_dir ]; then
+    rm -r $root_filtered_burst_noise_dir
+fi
+mkdir $root_filtered_burst_noise_dir
+
+real_filtered_burst_noise_dir='/workspaces/filtered_burst_noise_data_sets/real'
+if [ -d $real_filtered_burst_noise_dir ]; then
+    rm -r $real_filtered_burst_noise_dir
+fi
+mkdir $real_filtered_burst_noise_dir
+
+fake_filtered_burst_noise_dir='/workspaces/filtered_burst_noise_data_sets/fake'
+if [ -d $fake_filtered_burst_noise_dir ]; then
+    rm -r $fake_filtered_burst_noise_dir
+fi
+mkdir $fake_filtered_burst_noise_dir
+
+# Filter noise for real data
+python /workspaces/src/filter_noise_dataset.py $real_burst_noise_dataset_path $real_filtered_burst_noise_dir 
+
+# Filter noise for fake data
+python /workspaces/src/filter_noise_dataset.py $fake_burst_noise_dataset_path $fake_filtered_burst_noise_dir 
--- workflows/9_Collect_Metrics.sh
+++ workflows/9_Collect_Metrics.sh
@@ -8,7 +8,8 @@ count=${1:-$default}
 dataset_path='/workspaces/data_sets'
 white_noise_dataset_path='/workspaces/white_noise_data_sets'
 burst_noise_dataset_path='/workspaces/burst_noise_data_sets'
-filtered_noise_dataset_path='/workspaces/filtered_noise_data_sets'
+filtered_white_noise_dataset_path='/workspaces/filtered_white_noise_data_sets'
+filtered_burst_noise_dataset_path='/workspaces/filtered_burst_noise_data_sets'
 
 trained_model_path='/workspaces/trained_model'
 prediction_data_path='/workspaces/prediction_data'
@@ -24,7 +25,9 @@ mkdir $metrics_root_path/white_noise_train
 mkdir $metrics_root_path/burst_noise_train
 mkdir $metrics_root_path/white_noise_no_train
 mkdir $metrics_root_path/burst_noise_no_train
-mkdir $metrics_root_path/filtered_noise
+mkdir $metrics_root_path/filtered_white_noise
+mkdir $metrics_root_path/filtered_burst_noise
+
 
 for i in $(seq 1 $count)
 do
@@ -32,7 +35,8 @@ do
     python /workspaces/src/make_predictions.py $dataset_path -tmp $trained_model_path -pdp $prediction_data_path > $metrics_root_path/baseline/metric_$i.txt
     python /workspaces/src/make_predictions.py $white_noise_dataset_path -tmp $trained_model_path -pdp $prediction_data_path > $metrics_root_path/white_noise_no_train/metric_$i.txt
     python /workspaces/src/make_predictions.py $burst_noise_dataset_path -tmp $trained_model_path -pdp $prediction_data_path > $metrics_root_path/burst_noise_no_train/metric_$i.txt
-    python /workspaces/src/make_predictions.py $filtered_noise_dataset_path -tmp $trained_model_path -pdp $prediction_data_path > $metrics_root_path/filtered_noise/metric_$i.txt
+    python /workspaces/src/make_predictions.py $filtered_white_noise_dataset_path -tmp $trained_model_path -pdp $prediction_data_path > $metrics_root_path/filtered_white_noise/metric_$i.txt
+    python /workspaces/src/make_predictions.py $filtered_burst_noise_dataset_path -tmp $trained_model_path -pdp $prediction_data_path > $metrics_root_path/filtered_burst_noise/metric_$i.txt
     python /workspaces/src/fit_model.py $white_noise_dataset_path -tmp $trained_model_path
     python /workspaces/src/make_predictions.py $white_noise_dataset_path -tmp $trained_model_path -pdp $prediction_data_path > $metrics_root_path/white_noise_train/metric_$i.txt
     python /workspaces/src/fit_model.py $burst_noise_dataset_path -tmp $trained_model_path
@@ -43,12 +47,14 @@ echo 'Baseline Training and Prediction:'
 python /workspaces/src/parse_metrics.py $metrics_root_path/baseline
 echo '\nBaseline Training and White Noise Prediction:'
 python /workspaces/src/parse_metrics.py $metrics_root_path/white_noise_no_train
-echo '\nBaseline Training and Burst Noise Prediction:'
-python /workspaces/src/parse_metrics.py $metrics_root_path/burst_noise_no_train
-echo '\nBaseline Training and Filtered Noise Prediction:'
-python /workspaces/src/parse_metrics.py $metrics_root_path/filtered_noise
 echo '\nWhite Noise Training and Prediction:'
 python /workspaces/src/parse_metrics.py $metrics_root_path/white_noise_train
+echo '\nBaseline Training and Burst Noise Prediction:'
+python /workspaces/src/parse_metrics.py $metrics_root_path/filtered_white_noise
+echo '\nBaseline Training and Filtered Burst Noise Prediction:'
+python /workspaces/src/parse_metrics.py $metrics_root_path/burst_noise_no_train
 echo '\nBurst Noise Training and Prediction:'
 python /workspaces/src/parse_metrics.py $metrics_root_path/burst_noise_train
+echo '\nBaseline Training and Filtered White Noise Prediction:'
+python /workspaces/src/parse_metrics.py $metrics_root_path/filtered_burst_noise
 
